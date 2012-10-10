febcd5c6cb5ac829c475062c479216c91184539a
Rodolfo Finochietti <ml@pboard.com.ar>
2012-10-10 15:08:04 -0300

    More Windows 8 samples

---
+++ Windows 8/dotNet/BasicDemo/WinRTCSharp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WinRTCSharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var fileOpenPicker = new Windows.Storage.Pickers.FileOpenPicker();
+            fileOpenPicker.FileTypeFilter.Add(".jpeg");
+
+            var file = await fileOpenPicker.PickSingleFileAsync();
+
+            new Windows.UI.Popups.MessageDialog(file.Name, "File Picked").ShowAsync();
+
+            //.Completed = (asyncInfo, asyncStatus) =>
+            //    {
+            //        var file = asyncInfo.GetResults();
+            //        new Windows.UI.Popups.MessageDialog(file.Name, "File Picked").ShowAsync();
+            //    };
+        }
+    }
+}
