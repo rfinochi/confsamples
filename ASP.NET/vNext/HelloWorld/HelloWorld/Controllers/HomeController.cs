87acd98402bcc28bdc82766cfd562c207edeeb3a
Rodolfo Finochietti <rfinochi@gmail.com>
2014-06-29 12:07:13 -0300

    Roslyn and ASP.NET vNext Samples

---
+++ ASP.NET/vNext/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "vNext!";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET/vNext/HelloWorld/HelloWorld/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void Configure(IBuilder app)
+        {
+            // Setup configuration sources
+            var configuration = new Configuration();
+            configuration.AddJsonFile("config.json");
+            configuration.AddEnvironmentVariables();
+
+            // Set up application services
+            app.UseServices(services =>
+            {
+                // Add MVC services to the services container
+                services.AddMvc();
+            });
+
+            // Add static files to the request pipeline
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default", 
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+
+                routes.MapRoute(
+                    name: "api",
+                    template: "{controller}/{id?}");
+            });
+        }
+    }
+}
