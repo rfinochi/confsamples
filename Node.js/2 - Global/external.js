b5415acb74da8ddce131f02cc04c6930eb149da6
Rodolfo Finochietti <rfinochi@gmail.com>
2015-12-18 11:37:31 -0300

    Update to Node.js Samples

--- Node.js/2 - Global/external.js
+++
@@ -1,8 +0,0 @@
-// declaring variable without the var keyword
-// makes it global in JavaScript
-externalWithoutVar = 'without var';
-    
-// declaring a variable with the var keyword
-// outside of a function declaration adds it
-// to the window object in the browser.
-var externalWithVar = 'with var';
