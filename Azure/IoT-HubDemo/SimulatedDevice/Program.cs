4e4834eafa282b025b7957c43770b4cd1d6d64ae
Rodolfo Finochietti <rfinochi@gmail.com>
2016-05-17 08:57:01 -0300

    using reorder

--- Azure/IoT-HubDemo/SimulatedDevice/Program.cs
+++ Azure/IoT-HubDemo/SimulatedDevice/Program.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Azure.Devices.Client;
 using Newtonsoft.Json;
-using System.Threading;
 
 namespace SimulatedDevice
 {
