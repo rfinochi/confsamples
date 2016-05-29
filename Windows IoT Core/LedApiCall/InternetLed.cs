35ee47aa2c992c2e39a08cc47f76c262630dd832
Rodolfo Finochietti <rfinochi@gmail.com>
2016-05-29 12:57:27 -0300

    Windows IoT Core Samples

---
+++ Windows IoT Core/LedApiCall/InternetLed.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace LedApiCall
+{
+    /// <summary>
+    /// A class which wraps a GPIO interfaced LED which can get it's display state set based on
+    /// a web API.
+    /// </summary>
+    class InternetLed
+    {
+        // GPIO controller code
+        private GpioController gpio;
+        private GpioPin LedControlGPIOPin;
+        private int LedControlPin;
+
+        // only used if we don't get a response from the webapi call.
+        private const int DefaultBlinkDelay = 1000;
+
+        // An enumeration to store the state of the led
+        public enum eLedState { Off, On };
+        private eLedState _LedState;
+
+        public InternetLed(int ledControlPin)
+        {
+            Debug.WriteLine("InternetLed::New InternetLed");
+
+            // Store the selected GPIO control pin id for use when we initialize the GPIO
+            LedControlPin = ledControlPin;
+        }
+
+
+        public void InitalizeLed()
+        {
+            Debug.WriteLine("InternetLed::InitalizeLed");
+
+            // Now setup the LedControlPin
+            gpio = GpioController.GetDefault();
+
+            LedControlGPIOPin = gpio.OpenPin(LedControlPin);
+            LedControlGPIOPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            // Get the current pin value
+            GpioPinValue startingValue = LedControlGPIOPin.Read();
+            _LedState = (startingValue == GpioPinValue.Low) ? eLedState.On : eLedState.Off;
+        }
+
+        // A public property for interacting with the LED from code.
+        public eLedState LedState
+        {
+            get { return _LedState; }
+            set
+            {
+                Debug.WriteLine("InternetLed::LedState::set " + value.ToString());
+                if (LedControlGPIOPin != null)
+                {
+                    GpioPinValue newValue = (value == eLedState.On ? GpioPinValue.High : GpioPinValue.Low);
+                    LedControlGPIOPin.Write(newValue);
+                    _LedState = value;
+                }
+            }
+        }
+
+        // Change the state of the led from on to off or off to on
+        public void Blink()
+        {
+            if (LedState == eLedState.On)
+            {
+                LedState = eLedState.Off;
+            }
+            else
+            {
+                LedState = eLedState.On;
+            }
+        }
+
+        // This will call an exposed web API at the indicated URL
+        // the API will return a string value to use as the blink delay.
+        const string WebAPIURL = "http://adafruitsample.azurewebsites.net/TimeApi";
+        public async Task<int> GetBlinkDelayFromWeb()
+        {
+            Debug.WriteLine("InternetLed::MakeWebApiCall");
+
+            string responseString = "No response";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Make the call
+                    responseString = await client.GetStringAsync(WebAPIURL);
+
+                    // Let us know what the returned string was
+                    Debug.WriteLine(String.Format("Response string: [{0}]", responseString));
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            int delay;
+
+            if (!int.TryParse(responseString, out delay))
+            {
+                delay = DefaultBlinkDelay;
+            }
+
+            // return the blink delay
+            return delay;
+        }
+
+    }
+
+}
---
+++ Windows IoT Core/LedApiCall/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace LedApiCall
+{
+    /// <summary>
+    /// The application main page.  Because we are running headless we will not see anything
+    /// even though it is begin generated at runtime.  This acts as the main entry point for the 
+    /// application functionality.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // which GPIO pin do we want to use to control the LED light
+        const int GPIOToUse = 18;
+
+        // The class which wraps our LED.
+        InternetLed internetLed;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // This method will be called by the application framework when the page is first loaded.
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            MakePinWebAPICall();
+
+            try
+            {
+                // Create a new InternetLed object
+                internetLed = new InternetLed(GPIOToUse);
+
+                // Initialize it for use
+                internetLed.InitalizeLed();
+
+                // Now have it make the web API call and get the led blink delay
+                int blinkDelay = await internetLed.GetBlinkDelayFromWeb();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    internetLed.Blink();
+                    await Task.Delay(blinkDelay);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        // This method will put your pin on the world map of makers using this lesson.
+        // This uses imprecise location (for example, a location derived from your IP 
+        // address with less precision such as at a city or postal code level). 
+        // No personal information is stored.  It simply
+        // collects the total count and other aggregate information.
+        // http://www.microsoft.com/en-us/privacystatement/default.aspx
+        // Comment out the line below to opt-out
+        /// </summary>
+        public void MakePinWebAPICall()
+        {
+            try {
+                HttpClient client = new HttpClient();
+
+                // Comment this line to opt out of the pin map.
+                client.GetStringAsync("http://adafruitsample.azurewebsites.net/api?Lesson=201");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Web call failed: " + e.Message);
+            }
+        }
+
+    }
+}
